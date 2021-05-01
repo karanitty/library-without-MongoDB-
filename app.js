eb3433dd0faa746af0c4c089d04be27a9949cc00
karanitty <karanitty@gmail.com>
2021-05-01 21:23:33 +0530

    Third Commit

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 var chalk = require("chalk");
 const express = require("express");
-var login = express.Router();
-var signup = express.Router();
 
 var app = new express();
 const nav = [
@@ -48,28 +46,22 @@ const nav_authors = [
 
 var booksRouter = require('./src/routes/bookRoutes')(nav_books);
 var authorsRouter = require('./src/routes/authorRoutes')(nav_authors);
+var loginRouter = require('./src/routes/loginRoutes')(nav);
+var signupRouter = require('./src/routes/signupRoutes')(nav);
 
 app.set('view engine','ejs');
 app.set('views','./src/views');
 app.use(express.static('./public'));
 app.use('/books',booksRouter);
 app.use('/authors',authorsRouter);
-app.use('/login',login);
-app.use('/signup',signup);
+app.use('/login',loginRouter);
+app.use('/signup',signupRouter);
 
 app.get('/',function(req,res){
     res.render('index.ejs',
     {
         title:'Library',
         nav,
-    });
-});
-
-login.get('/',function(req,res){
-    res.render('login',
-    {
-        title:'Log In',
-        nav
     });
 });
 
