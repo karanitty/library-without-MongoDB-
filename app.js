4c0e2c1df2c8e3bec0b464cef0a94e8df273a110
karanitty <karanitty@gmail.com>
2021-05-01 20:56:49 +0530

    Second Commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var chalk = require("chalk");
 const express = require("express");
-var signin = express.Router();
+var login = express.Router();
 var signup = express.Router();
 
 var app = new express();
@@ -13,37 +13,60 @@ const nav = [
         link:'/authors',
         name:'Author'
     },
-    {
-        link:'/signin',
-        name:'Log In'
-    },
-    {
-        link:'/signup',
-        name:'Sign Up'
-    },
 ];
 
-var booksRouter = require('./src/routes/bookRoutes')(nav);
-var authorsRouter = require('./src/routes/authorRoutes')(nav);
+const nav_books = [
+    {
+        link:'/books',
+        name:'Books'
+    },
+    {
+        link:'/authors',
+        name:'Author'
+    },
+    {
+        link:'/add_book',
+        name: 'Add Book'
+    }
+];
+
+const nav_authors = [
+    {
+        link:'/books',
+        name:'Books'
+    },
+    {
+        link:'/authors',
+        name:'Author'
+    },
+    {
+        link:'/add_author',
+        name: 'Add Author'
+    }
+];
+
+
+var booksRouter = require('./src/routes/bookRoutes')(nav_books);
+var authorsRouter = require('./src/routes/authorRoutes')(nav_authors);
 
 app.set('view engine','ejs');
 app.set('views','./src/views');
 app.use(express.static('./public'));
 app.use('/books',booksRouter);
 app.use('/authors',authorsRouter);
-app.use('/signin',signin);
+app.use('/login',login);
 app.use('/signup',signup);
 
 app.get('/',function(req,res){
     res.render('index.ejs',
     {
         title:'Library',
-        nav
+        nav,
     });
 });
 
-signin.get('/',function(req,res){
-    res.render('signin',
+login.get('/',function(req,res){
+    res.render('login',
     {
         title:'Log In',
         nav
